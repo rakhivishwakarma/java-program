7f5d24dd3bf22f6ae4493a56a324a43367e05259
rakhivishwakarma <52989439+rakhivishwakarma@users.noreply.github.com>
2019-08-06 23:01:18 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,16 @@
+import java.util;
+class Calc
+{
+ public static void main(String args[])
+ {
+  int a=4;
+  double sq=Math.pow(a,2);
+  double sq=Math.sqrt(a);
+  double sq=Math.pow(a,3);
+  double sq=Math.cbrt((a);
+  System.out.print("Square="+sq);
+  System.out.print("Square root="+square);
+  System.out.print("Cube="+cb);
+  System.out.print("Cube root="+cube);
+ }
+}
