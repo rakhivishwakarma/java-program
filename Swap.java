7c48843c2d54d8bc2c378c26d4f8cefc42bed586
rakhivishwakarma <52989439+rakhivishwakarma@users.noreply.github.com>
2019-08-06 23:03:24 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+class Swap
+{
+  public static void main(String args[])
+ {
+   int a=3,b=5;
+   int temp;
+   System.out.println("Before Swap:a="+a);
+   System.out.println("After Swap:a="+b);
+   temp=a;
+   a=b;
+   b=temp;
+   System.out.println("Before Swap:b="+b);
+   System.out.println("After Swap:b="+a);
+  }
+}
+ 
