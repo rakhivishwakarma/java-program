cef20e611bd7e52e99ed76de921f3db9f2087fe3
rakhivishwakarma <52989439+rakhivishwakarma@users.noreply.github.com>
2019-08-06 21:34:23 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,12 @@
+class Area
+{
+ public static void main(String args[])
+ {
+  double area,circ,rad;
+  rad=5;
+  area=3.14*rad*rad;
+  circ=2*3.14*rad;
+  System.out.println("area="+area);
+  System.out.println("circumference="+circ);
+ }
+}
