2c67ded4e1ed2c2b96c73da7515c0ce9f06f15a5
rakhivishwakarma <52989439+rakhivishwakarma@users.noreply.github.com>
2019-08-25 17:08:07 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle
+{
+ public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter lenth");
+  double l=sc.nextDouble():
+  System.out.println("Enter breath");
+  double b=sc.nextDouble();
+  double area=l*b;
+  System.out.println(Area of rectangle="+area);
+ }
+}
