779cc9780f9d2e64eafc9cbfd6da8a28927a8794
rakhivishwakarma <52989439+rakhivishwakarma@users.noreply.github.com>
2019-08-25 17:10:34 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Name
+{
+ public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter your name");
+  String str=sc.next();
+  System.out.println("Your name"+" "+str);
+ }
+}
